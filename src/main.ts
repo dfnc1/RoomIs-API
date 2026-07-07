c0bccb599e961ee0f44cfacf3f9f9501ec13d0ec
dfnc1 <dianfnc06@gmail.com>
2026-07-07 20:15:25 +0700

    feat: add validation pipe and json logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConsoleLogger, ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule, {
+        logger: new ConsoleLogger({
+            json: true,
+        }),
+    });
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        transform: true,
+    }));
+    await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+    console.error(err)
+});
