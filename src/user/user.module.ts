eb73aa932f90b97ced51b528831bc2c94895e151
dfnc1 <dianfnc06@gmail.com>
2026-07-10 23:19:22 +0700

    feat: add auth and prisma module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [AuthModule, PrismaModule],
   controllers: [UserController],
   providers: [UserService],
 })
