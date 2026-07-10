26be40f961a41c740dda02fb2103c4ab8d329589
dfnc1 <dianfnc06@gmail.com>
2026-07-10 23:21:19 +0700

    feat: implement global interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {ConsoleLogger, ValidationPipe} from "@nestjs/common";
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  ConsoleLogger,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        logger: new ConsoleLogger({
-            json: true,
-        }),
-    });
-    app.useGlobalPipes(new ValidationPipe({
-        whitelist: true,
-        transform: true,
-    }));
-    await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      json: true,
+    }),
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap().catch((err) => {
-    console.error(err)
+  console.error(err);
 });
