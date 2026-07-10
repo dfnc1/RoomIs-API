6925bed33256a679f387628a13a300ad8eee7cd0
dfnc1 <dianfnc06@gmail.com>
2026-07-10 23:09:06 +0700

    fix: resolve wrong import

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,14 @@
-import {Injectable} from '@nestjs/common';
-import {PrismaClient} from "@prisma/client/extension";
-import {PrismaPg} from "@prisma/adapter-pg";
-import {ConfigService} from "@nestjs/config";
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(private configService: ConfigService) {
-        const adapter = new PrismaPg({connectionString: configService.get<string>('DATABASE_URL')});
-        super({ adapter });
-    }
+  constructor(private configService: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: configService.get<string>('DATABASE_URL'),
+    });
+    super({ adapter });
+  }
 }
