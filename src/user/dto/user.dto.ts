f1e289edfcdd791ceb122e0c1742243f28d5d4a5
dfnc1 <dianfnc06@gmail.com>
2026-07-10 23:11:47 +0700

    feat: update user dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,49 +1,51 @@
-import {IsEmail, IsNotEmpty, IsOptional, IsString, MinLength} from "class-validator";
-import {Exclude, Transform} from "class-transformer";
-import {PartialType, PickType} from "@nestjs/mapped-types";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Exclude, Transform } from 'class-transformer';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 
 export class RegisterDto {
-    @IsNotEmpty()
-    @IsString()
-    name: string;
+  @IsNotEmpty()
+  @IsString()
+  name: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(8)
-    password: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password: string;
 }
 
-export class LoginDto extends PickType(RegisterDto, ['email', 'password'] as const) {
+export class LoginDto extends PickType(RegisterDto, [
+  'email',
+  'password',
+] as const) {}
+
+export class UpdateUserDto extends PartialType(RegisterDto) {}
+
+export class AuthResponse {
+  token_type: 'Bearer';
+  access_token: string;
 }
 
-export class UpdateUserDto extends PartialType(RegisterDto) {
-}
+export class UserResponse {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
 
-export class AuthResponseDto {
-    token_type: "Bearer";
-    access_token: string;
-}
+  @Transform(({ value }) => value?.toISOString())
+  createdAt: Date;
 
-export class UserResponseDto {
-    id: string
-    name: string
-    email: string
-    role: string
+  @Transform(({ value }) => value?.toISOString())
+  updatedAt: Date;
 
-    @Transform(({value}) => value?.toISOString())
-    createdAt: Date;
+  @Exclude()
+  password: string;
 
-    @Transform(({value}) => value?.toISOString())
-    updatedAt: Date;
-
-    @Exclude()
-    password: string;
-
-    constructor(partial: Partial<UserResponseDto>) {
-        Object.assign(this, partial)
-    }
+  constructor(partial: Partial<UserResponse>) {
+    Object.assign(this, partial);
+  }
 }
