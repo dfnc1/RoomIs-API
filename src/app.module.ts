6a6d4de23b990b76f6395ba5ce39d3323cd685f7
dfnc1 <dianfnc06@gmail.com>
2026-07-15 18:47:38 +0700

    feat: add new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { AssetModule } from './asset/asset.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
   imports: [
-      PrismaModule,
-      ConfigModule.forRoot({
-        isGlobal: true,
-      })
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+    AssetModule,
+    BookingModule,
   ],
   controllers: [],
   providers: [PrismaService],
