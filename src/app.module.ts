bb9b7121b7a8262bd63ba937710666dedc69f0cc
dfnc1 <dianfnc06@gmail.com>
2026-07-15 18:48:06 +0700

    feat: add schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth/auth.module';
 import { AssetModule } from './asset/asset.module';
 import { BookingModule } from './booking/booking.module';
@@ -13,6 +14,7 @@ import { BookingModule } from './booking/booking.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     UserModule,
     AuthModule,
     AssetModule,
